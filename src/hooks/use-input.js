05809c97494610ef9f3fe7a3aff4a63be5c8d52f
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-01-19 13:18:57 +0100

    Created Input validator hook.

---
+++ src/hooks/use-input.js
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+
+const initialInputState = {
+    value: '',
+    isTouched: false
+}
+
+const inputReducer = (state, action) => {
+    if (action.type === 'BLUR') {
+        return {isTouched: true, value: action.value};
+    } else if (action.type === 'INPUT') {
+        return {isTouched: state.isTouched, value: state.value};
+    }
+    else if (action.type === 'RESET') {
+        return initialInputState;
+    }
+    return inputReducer;
+}
+
+const useInput = (validator) => {
+    const [inputState, dispatch] = useReducer(inputReducer, initialInputState);
+
+    const valueIsValid = validator(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
+
+    const valueChangeHandler = (event) => {
+        dispatch({type: 'INPUT', value: event.target.value});
+    }
+
+    const inputBlurHandler = (event) => {
+        dispatch({type: 'BLUR'});
+    }
+
+    const inputReset = () => {
+        dispatch({type: 'RESET'});
+    }
+
+    return {
+        value: inputState.value,
+        isValid: valueIsValid,
+        hasError,
+        valueChangeHandler,
+        inputBlurHandler,
+        inputReset
+    };
+};
+
+export default useInput;
