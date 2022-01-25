4b79faa88ca4ac620ddd235bf46d5f78b0744f95
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-01-25 23:18:04 +0100

    Created resources context.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,26 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
 import Layout from "../components/Layout/Layout";
+import useHttp from "../hooks/use-http";
 import AuthContext from "../store/auth-context"
+import { ResourcesContextProvider } from "../store/resources-context";
 
 const HomePage = () => {
     const authCtx = useContext(AuthContext);
     const isLoggedIn = authCtx.isLoggedIn;
 
+    useEffect(() => {
+        //sendRequest('http://localhost:8000/api/resources/userresources/', 'GET', authCtx.data.token, setResources)
+    })
+
     if (!isLoggedIn) {
         return <Navigate to='/auth' />
     }
     
-    return <Layout />
+    return <ResourcesContextProvider>
+            <Layout />
+        </ResourcesContextProvider>
 }
 
 export default HomePage;
---
+++ src/store/resources-context.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+const ResourcesContext = React.createContext({
+    characters: [],
+    items: [],
+    setCharacters: (data) => {},
+    setItems: (data) => {}
+});
+
+export const ResourcesContextProvider = (props) => {
+    const tmpItems = localStorage.getItem('items') === null ? [] : JSON.parse(localStorage.getItem('items'));
+    const tmpCharacters = localStorage.getItem('characters') === null ? [] : JSON.parse(localStorage.getItem('characters'));
+
+    const [items, setItems] = useState(tmpItems);
+    const [characters, setCharacters] = useState(tmpCharacters);
+
+    const setCharactersHandler = (data) => {
+        const tmpChars = [];
+        data.forEach(element => {
+            tmpChars.push(element);
+        });
+        setCharacters(tmpChars);
+        localStorage.setItem('characters', JSON.stringify(tmpChars));
+    }
+
+    const setItemsHandler = (data) => {
+        const tmpItems = [];
+        data.forEach(element => {
+            tmpItems.push(element);
+        });
+        setItems(tmpItems);
+        localStorage.setItem('items', JSON.stringify(tmpItems));
+    }
+
+    const contextValue = {
+        characters: characters,
+        items: items,
+        setCharacters: setCharactersHandler,
+        setItems: setItemsHandler
+    };
+
+    return <ResourcesContext.Provider value={contextValue}>{props.children}</ResourcesContext.Provider>
+};
+
+export default ResourcesContext;
