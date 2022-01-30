2803db85dceb049c8d5c2af2c3bf301449711596
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-01-30 14:12:33 +0100

    Added handler for adding new characters.
    - Handler adds new characters, duplicates aren't included.

--- src/store/resources-context.js
+++ src/store/resources-context.js
@@ -4,6 +4,7 @@ const ResourcesContext = React.createContext({
     characters: [],
     items: [],
     setCharacters: (data) => {},
+    addCharacters: (data) => {},
     setItems: (data) => {}
 });
 
@@ -13,6 +14,18 @@ export const ResourcesContextProvider = (props) => {
 
     const [items, setItems] = useState(tmpItems);
     const [characters, setCharacters] = useState(tmpCharacters);
+
+    const getUniqueValues = (arr) => {
+        const tmp = arr.concat();
+        for (let i = 0; i < tmp.length; ++i) {
+            for (let j = i+1; j < tmp.length; ++j) {
+                if (tmp[i].id === tmp[j].id) {
+                    tmp.splice(j--, 1);
+                }
+            }
+        }
+        return tmp;
+    }
 
     const setCharactersHandler = (data) => {
         const tmpChars = [];
@@ -32,10 +45,17 @@ export const ResourcesContextProvider = (props) => {
         localStorage.setItem('items', JSON.stringify(tmpItems));
     }
 
+    const addCharactersHandler = (data) => {
+        const tmpNewChars = data.concat(characters);
+        const dataToSet = getUniqueValues(tmpNewChars);
+        setCharacters(dataToSet);
+    }
+
     const contextValue = {
         characters: characters,
         items: items,
         setCharacters: setCharactersHandler,
+        addCharacters: addCharactersHandler,
         setItems: setItemsHandler
     };
 
