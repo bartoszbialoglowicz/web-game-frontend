eceb4bc05c84bdf306e773bf6df0d95cc6392588
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-01-31 12:17:24 +0100

    Item list stylized.

--- src/components/Collection/ItemList.js
+++ src/components/Collection/ItemList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Item from './Item';
+import './ItemList.css';
 
 const ItemList = props => {
     const list = props.list.map(el => <Item item={el} key={el.id} />)
-    return <div>
-        <h3>{props.title}</h3>
+    return <div className='list-item'>
+        <h1>{props.title}</h1>
         {list}
     </div>;
 };
