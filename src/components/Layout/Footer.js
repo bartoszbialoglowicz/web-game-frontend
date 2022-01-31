130422358bd3a3544f0ca15ae09a616a08bd6eac
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-01-31 12:17:11 +0100

    Icon made by flaticon

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {
   return <div>
       <p>demo game &copy; Bartosz Białogłowicz 2021</p>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/designvector10" title="designvector10">designvector10</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   </div>;
 };
 
