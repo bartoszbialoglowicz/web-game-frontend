8cb1108a1c00a983729c06397241dfedb1261795
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-02 01:32:07 +0100

    Added css.

--- src/components/Store/Chest.js
+++ src/components/Store/Chest.js
@@ -1,11 +1,12 @@
 import Button from "../UI/Button";
+import classes from './Chest.module.css';
 
 const Chest = props => {
 
-    return <div key={props.element.id}>
+    return <div key={props.element.id} className={classes.chest}>
           <p>{props.element.name}</p>
           <p>Cost: {props.element.price === 0 ? 'Free' : props.element.price}</p>
-          <form onSubmit={(event) =>{
+          <form className={classes.chestForm}onSubmit={(event) =>{
             event.preventDefault();
             props.onSubmit(props.chances, props.element.quantity)}}>
             <Button 
