33cff0724a3bd15305d1e7611ee2dfaf01059601
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-02 01:30:45 +0100

    Navbar stylized.
    - Added icons
    - Added flexibility
    - Stylized and reformated html structure.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,28 +1,57 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Header.module.css';
+import cancel from '../../assets/icons/cancel.png';
+import { useState } from 'react/cjs/react.development';
+
+import list from '../../assets/icons/menu.png';
+import signOut from '../../assets/icons/sign-out.png';
+import home from '../../assets/icons/tent.png';
+import quests from '../../assets/icons/certificate.png';
+import store from '../../assets/icons/treasure.png';
+import collection from '../../assets/icons/card-game.png';
 
 const Header = () => {
+    const [isHiden, setIsHiden] = useState(false);
     const authCtx = useContext(AuthContext);
+
+    let hideClass = isHiden ? classes.hidden : '';
+
+    const hideElementHandler = () => {
+        setIsHiden(!isHiden);
+    }
     
     const logoutHandler = () => {
         authCtx.logout();
     }
 
     return (
-      <header className={classes.nav}>
-          <div><p>{authCtx.data.name}</p></div>
-          <div><p onClick={logoutHandler}>Sign Out</p></div>
-          <div>Gold: {authCtx.data.gold}</div>
-          <div>LVL: {authCtx.data.lvl}</div>
-          <div>XP: {authCtx.data.experience}</div>
-          <Link to={'/'}><div><p>Home</p></div></Link>
-          <Link to={'quests'}><div><p>Quests</p></div></Link>
-          <Link to={'store'}><div><p>Store</p></div></Link>
-          <Link to={'collection'}><div><p>Collection</p></div></Link>
-      </header>
+        <Fragment>
+            <header className={classes.nav + ' ' + hideClass}>
+                <div className={classes.inner + ' ' + hideClass}>
+                    <div className={classes.hideIcon} onClick={hideElementHandler}>
+                        <img src={cancel} alt="close"></img>
+                    </div>
+                    <ul>
+                        <li><p>Welcome, {authCtx.data.name}</p>
+                        <p>Gold: {authCtx.data.gold}</p>
+                        <p>LVL: {authCtx.data.lvl}</p>
+                        <p>XP: {authCtx.data.experience}</p></li>
+                        <li><img src={home} alt='home' /><Link to={'/'}>Home</Link></li>
+                        <li><img src={quests} alt='quests' /><Link to={'quests'}>Quests</Link></li>
+                        <li><img src={store} alt='store' /><Link to={'store'}>Store</Link></li>
+                        <li><img src={collection} alt='collection' /><Link to={'collection'}>Collection</Link></li>
+                        <li className={classes.signOut} onClick={logoutHandler}><img src={signOut} alt='signout'/><p>Sign Out</p></li>
+                    </ul>
+                </div>
+            </header>
+            <div className={!isHiden ? classes.hidden : classes.showIcon} onClick={hideElementHandler}>
+                <img src={list} alt="menu" />
+            </div>
+        </Fragment>
+      
   );
 };
 
