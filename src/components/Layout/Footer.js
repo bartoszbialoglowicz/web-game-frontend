c9ccb19761e9afa7b8a740e4a2c40e7e8f91c67e
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-02 01:29:10 +0100

    Updated footer template.

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './Footer.css';
+
 const Footer = () => {
-  return <div>
+  return <footer>
       <p>demo game &copy; Bartosz Białogłowicz 2021</p>
       <div>Icons made by <a href="https://www.flaticon.com/authors/designvector10" title="designvector10">designvector10</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-  </div>;
+  </footer>;
 };
 
 export default Footer;
