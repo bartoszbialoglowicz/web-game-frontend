d0cea8a8a01e6804846f3ac0eae8014fff15419b
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-03 19:39:03 +0100

    Added copyrights.

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,7 +5,11 @@ import './Footer.css';
 const Footer = () => {
   return <footer>
       <p>demo game &copy; Bartosz Białogłowicz 2021</p>
-      <div>Icons made by <a href="https://www.flaticon.com/authors/designvector10" title="designvector10">designvector10</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div>Icons made by 
+        <a href="https://www.flaticon.com/authors/designvector10" title="designvector10">designvector10</a> from 
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>
+        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        <a href="https://www.flaticon.com/free-icons/team" title="team icons">Team icons created by Freepik - Flaticon</a>
   </footer>;
 };
 
