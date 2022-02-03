c64c2a6390d2f11fa3af12cc6e874aad639631a7
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-03 19:37:05 +0100

    Added new item to the list.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Header.module.css';
 import cancel from '../../assets/icons/cancel.png';
-import { useState } from 'react/cjs/react.development';
 
 import list from '../../assets/icons/menu.png';
 import signOut from '../../assets/icons/sign-out.png';
 import home from '../../assets/icons/tent.png';
 import quests from '../../assets/icons/certificate.png';
 import store from '../../assets/icons/treasure.png';
+import team from '../../assets/icons/support.png';
 import collection from '../../assets/icons/card-game.png';
 import ResourcesContext from '../../store/resources-context';
 
@@ -44,6 +44,7 @@ const Header = () => {
                         <li><img src={home} alt='home' /><Link to={'/'}>Home</Link></li>
                         <li><img src={quests} alt='quests' /><Link to={'quests'}>Quests</Link></li>
                         <li><img src={store} alt='store' /><Link to={'store'}>Store</Link></li>
+                        <li><img src={team} alt='team' /><Link to={'team'}>Team</Link></li>
                         <li><img src={collection} alt='collection' /><Link to={'collection'}>Collection</Link></li>
                         <li className={classes.signOut} onClick={logoutHandler}><img src={signOut} alt='signout'/><p>Sign Out</p></li>
                     </ul>
