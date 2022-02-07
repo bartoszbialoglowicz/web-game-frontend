3af504109a8b30249f942482de3b2b2f60a4cc7e
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2022-02-07 20:59:19 +0100

    Small css changes.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+
 const Home = () => {
-    return <React.Fragment>
+    return <Card>
         <h1>DEMO GAME</h1>
         <p>This is demo browser-based game. Project is is still in development phase, 
             so there are still functionalities that could not work properly.</p>
         <p>This is non-commercial project, so I do not profit from it.</p>
         <p>If you want report a bug or get any information about it contact me via email.</p>
         <p>Hope you will enjoy the adventure!</p>
-    </React.Fragment>;
+    </Card>
 };
 
 export default Home;
